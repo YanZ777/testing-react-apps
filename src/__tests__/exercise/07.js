0502e497d521f09332c0634e114b5f91ee23c414
Yan <yanzhao777@gmail.com>
2021-12-13 21:05:08 -0800

    Complete Exercise 7, Extra Credit 2
    - extract out custom render that can be reused by passing different
    options

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,13 +6,18 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-test('renders with the light styles for the light theme', () => {
-   function Wrapper({children}) {
-      return <ThemeProvider initialTheme="light">{children}</ThemeProvider>
-   }
 
+
+function customRender(ui, {theme, ...options}) {
+   function Wrapper({children}) {
+      return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+   }
+   return render(ui, {wrapper: Wrapper, ...options});
+}
+
+test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  customRender(<EasyButton>Easy</EasyButton>, {theme: "light"})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
@@ -24,11 +29,8 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the light styles for the light theme', () => {
-   function Wrapper({children}) {
-      return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
-   }
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  customRender(<EasyButton>Easy</EasyButton>, {theme: "dark"})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
