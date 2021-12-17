e3f0b88e83a3da51ad04f1c2eaaaf066e1903c03
Yan <yanzhao777@gmail.com>
2021-12-17 00:37:01 -0800

    Complete Exercise 8
    - create custom component to test custom hook

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -12,11 +12,34 @@ import useCounter from '../../components/use-counter'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
+const CustomHookTester = () => {
+   const { count, increment, decrement } = useCounter();
+   return (
+      <div>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <div>{count}</div>
+      </div>
+   );
+}
+
 test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the component
   // 🐨 get the elements you need using screen
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  render(<CustomHookTester />);
+
+  const incrementButton = screen.getByRole('button', { name: /increment/ });
+  const decrementButton = screen.getByRole('button', { name: /decrement/ });
+  expect(screen.getByText(/0/)).toBeInTheDocument();
+
+  userEvent.click(incrementButton);
+  expect(screen.getByText(/1/)).toBeInTheDocument();
+  
+  userEvent.click(decrementButton);
+  userEvent.click(decrementButton);
+  expect(screen.getByText(/-1/)).toBeInTheDocument();
 })
 
 /* eslint no-unused-vars:0 */
